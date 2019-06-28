26a83c90156c48a8e0a920d7048e7f006d1212bc
William Kent <wjk011@gmail.com>
2019-06-28 16:19:46 -0400

    [STORPORT] Copy missing declarations into precomp.h

--- drivers/storage/port/storport/precomp.h
+++ drivers/storage/port/storport/precomp.h
@@ -32,6 +32,27 @@
 #define TAG_INQUIRY_DATA    'QItS'
 #define TAG_SENSE_DATA      'NStS'
 
+/* From sdk/include/ddk/scsi.h */
+#define SCSI_SENSE_ILLEGAL_REQUEST        0x05
+typedef struct _SENSE_DATA {
+    UCHAR ErrorCode:7;
+    UCHAR Valid:1;
+    UCHAR SegmentNumber;
+    UCHAR SenseKey:4;
+    UCHAR Reserved:1;
+    UCHAR IncorrectLength:1;
+    UCHAR EndOfMedia:1;
+    UCHAR FileMark:1;
+    UCHAR Information[4];
+    UCHAR AdditionalSenseLength;
+    UCHAR CommandSpecificInformation[4];
+    UCHAR AdditionalSenseCode;
+    UCHAR AdditionalSenseCodeQualifier;
+    UCHAR FieldReplaceableUnitCode;
+    UCHAR SenseKeySpecific[3];
+} SENSE_DATA, *PSENSE_DATA;
+/* End code from scsi.h */
+
 typedef enum
 {
     dsStopped,
